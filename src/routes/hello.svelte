64cd11355cc050278a0d59979a722639537ed0e6
小倉裕一 <j211025ogu@gmail.com>
2019-05-23 17:58:25 +0900

    first commit

---
+++ src/routes/hello.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+  export function preload({ query }) {
+    return { query }
+  }
+</script>
+<script>
+  let name = 'World!'
+  export let query
+  if (query.name) {
+    name = query.name
+  }
+</script>
+
+<h1>Hello, {name}</h1>
